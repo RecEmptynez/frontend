adf4358d23c5e3b028f2ee0a156ca94d4c370055
Jonsson01 <emijon01@gmail.com>
2023-02-13 12:26:24 +0100

    Initial routing and sample wireframe added

---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,21 @@
+import { List } from "../components/List";
+
+export const MainPage = () => {
+  return (
+    <div className="w-screen h-screen flex flex-col items-center">
+      <h1 className="font-semibold text-7xl">Vad ska jag laga för mat?</h1>
+      <div className="w-full h-full">
+        <div className="w-full h-1/3 flex flex-col items-center justify-center">
+          <input
+            type="text"
+            className="rounded-2xl bg-slate-200 w-1/3 text-2xl p-2"
+            placeholder="Sök efter din ingrediens här!"
+          />
+        </div>
+        <div className="h-2/3 flex flex-col items-center justify-center">
+          <List />
+        </div>
+      </div>
+    </div>
+  );
+};
