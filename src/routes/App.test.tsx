beba1d7a6929b3482f7c24ffdf31ad864e20a758
Emil Jonsson <emijon01@gmail.com>
2023-02-21 15:13:18 +0100

    Simple initial test to test CI

--- src/routes/App.test.tsx
+++ src/routes/App.test.tsx
@@ -1,9 +1,8 @@
-import React from "react";
 import { render, screen } from "@testing-library/react";
-import App from "./routes/App";
+import App from "./App";
 
 test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Vad ska jag laga för mat/i);
   expect(linkElement).toBeInTheDocument();
 });
