0bc8466b6b652e7e83c99ea1f6a17e5815063002
Jonsson01 <emijon01@gmail.com>
2023-03-09 18:30:18 +0100

    Add functionality to remove ingredient from list

--- src/components/IngredientsList.tsx
+++ src/components/IngredientsList.tsx
@@ -2,30 +2,41 @@ import { ReactComponent as RemoveIngredientIcon } from "../assets/svgs/RemoveIng
 
 interface IngredientsListProp {
   ingredients: string[];
+  setIngredients: (value: string[]) => void;
 }
 
 export const IngredientsList = (props: IngredientsListProp) => {
-  const { ingredients } = props;
+  const { ingredients, setIngredients } = props;
+
+  const removeIngredient = (ingredient: string) => {
+    setIngredients(
+      ingredients.filter(
+        (removedIngredient) => removedIngredient !== ingredient
+      )
+    );
+  };
 
   return (
-    <div className="flex flex-col overflow-y-scroll rounded-[20px] border border-beige-1000 bg-beige-200 w-[385px] h-[386px]">
-      {ingredients.map((ingredient) => (
-        <div>
-          <div className="px-[30px] py-[25px] flex justify-between">
+    <div className="flex flex-col overflow-y-auto rounded-[20px] border border-beige-1000 bg-beige-200 w-[385px] h-[386px]">
+      {ingredients.map((ingredient, index) => (
+        <div key={index}>
+          <div className="px-[30px] py-[25px] flex justify-between text-beige-1200">
             {ingredient}
-            <div className="group flex items-center hover:cursor-pointer">
-              <RemoveIngredientIcon
-                onClick={() => console.log("Removed")}
-                className="group-hover:-translate-x- transition-all"
-              />
+            <div
+              onClick={() => removeIngredient(ingredient)}
+              className="group flex items-center hover:cursor-pointer"
+            >
+              <RemoveIngredientIcon className="group-hover:-translate-x- transition-all" />
               <p className="text-primary-red ml-2 text-[0px] group-hover:text-base transition-all duration-300">
                 Ta bort
               </p>
             </div>
           </div>
-          <div className="flex justify-center px-[30px]">
-            <div className={`h-[1px] w-full bg-beige-600`} />
-          </div>
+          {index < ingredients.length - 1 && (
+            <div className="flex justify-center px-[30px]">
+              <div className={`h-[1px] w-full bg-beige-600`} />
+            </div>
+          )}
         </div>
       ))}
     </div>
