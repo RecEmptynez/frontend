fbd405b3933249532e241d73e5c4c28cca8b4ece
Philip Winsnes <philip.winsnes@icloud.com>
2023-03-13 20:23:48 +0100

    Add pliancy

--- src/pages/RecipePage.tsx
+++ src/pages/RecipePage.tsx
@@ -4,14 +4,14 @@ import { arrowBackOutline, chevronDown, filterCircle } from 'ionicons/icons';
 export const RecipePage = () => {
 	return (
 		<div className="mx-10">
-			<div className="flex flex-row items-center my-10">
+			{/* Back Button */}
+			<div className="inline-flex flex-row items-center my-10 text-beige-1400 font-semibold text-base hover:text-beige-1000 hover:duration-300 cursor-pointer">
 				<IonIcon icon={arrowBackOutline} size="large" />
-				<p>Tillbaka</p>
+				Tillbaka
 			</div>
 
 			{/* Header */}
 			<div className="flex flex-row items-center justify-between">
-				{/* Left */}
 				<div className="flex flex-row items-center space-x-24">
 					<p className="font-bold text-3xl">Receptförslag</p>
 					<p className="">
@@ -19,13 +19,18 @@ export const RecipePage = () => {
 					</p>
 				</div>
 
-				{/* Right */}
 				<div className="flex flex-row items-center space-x-24">
-					<div className="flex flex-row space-x-2 items-center space-x">
+					{/* Sorting */}
+					<div className="flex flex-row space-x-2 items-center space-x text-beige-1200 hover:text-beige-1000 hover:duration-300 cursor-pointer text-sm">
 						<p>Sortera efter</p>
 						<IonIcon icon={chevronDown} />
 					</div>
-					<IonIcon icon={filterCircle} size="large" />
+					{/* Filter */}
+					<IonIcon
+						className="text-beige-1200 hover:text-beige-1000 cursor-pointer hover:duration-300"
+						icon={filterCircle}
+						size="large"
+					/>
 				</div>
 
 				{/* Add list of cards */}
