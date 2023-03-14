0f8fd935d84be4b56791035aca1e8048d5202444
Philip Winsnes <philip.winsnes@icloud.com>
2023-03-15 00:37:47 +0100

    REC-65: Add visible list on hover

--- src/pages/RecipePage.tsx
+++ src/pages/RecipePage.tsx
@@ -1,5 +1,12 @@
+import { useState } from 'react';
 import { IonIcon } from '@ionic/react';
-import { arrowBackOutline, chevronDown, filterCircle } from 'ionicons/icons';
+import {
+	arrowBackOutline,
+	checkbox,
+	chevronDown,
+	filterCircle,
+	squareOutline,
+} from 'ionicons/icons';
 
 export const RecipePage = () => {
 	return (
@@ -26,15 +33,71 @@ export const RecipePage = () => {
 						<IonIcon icon={chevronDown} />
 					</div>
 					{/* Filter */}
-					<IonIcon
-						className="text-beige-1200 hover:text-beige-1000 cursor-pointer hover:duration-300"
-						icon={filterCircle}
-						size="large"
-					/>
+					<Filtering />
 				</div>
 
 				{/* Add list of cards */}
 			</div>
+		</div>
+	);
+};
+
+const Filtering = () => {
+	const [isHovering, setIsHovering] = useState(false);
+
+	const handleMouseEnter = () => {
+		setIsHovering(true);
+	};
+
+	const handleMouseLeave = () => {
+		// setIsHovering(false);
+	};
+
+	return (
+		<div
+			className="relative"
+			onMouseEnter={handleMouseEnter}
+			onMouseLeave={handleMouseLeave}
+		>
+			<IonIcon
+				className="text-beige-1200 hover:text-beige-1000 cursor-pointer hover:duration-300"
+				icon={filterCircle}
+				size="large"
+			/>
+			{isHovering && (
+				<div className="absolute top-12 px-6 right-0 bg-beige-200 p-4 rounded-xl drop-shadow-md space-y-1">
+					<ListItem name="Vegetariskt" />
+					<ListItem name="Veganskt" />
+					<ListItem name="Glutenfritt" />
+					<ListItem name="Laktosfritt" />
+				</div>
+			)}
+		</div>
+	);
+};
+
+interface ListItemProps {
+	name: string;
+}
+
+const ListItem = (props: ListItemProps) => {
+	const { name } = props;
+	const [isChecked, setIsChecked] = useState(false);
+
+	const handleCheckboxChange = () => {
+		setIsChecked(!isChecked);
+	};
+
+	return (
+		<div className="flex flex-row space-x-2 items-center">
+			<div onClick={handleCheckboxChange} className="pt-1.5">
+				{isChecked ? (
+					<IonIcon icon={checkbox} style={{ 'font-size': 24 }} />
+				) : (
+					<IonIcon icon={squareOutline} style={{ 'font-size': 24 }} />
+				)}
+			</div>
+			<p className="text-sm">{name}</p>
 		</div>
 	);
 };
