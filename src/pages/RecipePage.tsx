88384d6f815a0dfbf43e712fe31a05b5bc6e8042
Philip Winsnes <philip.winsnes@icloud.com>
2023-03-14 16:43:41 +0100

    Done with REC-63 (#7)
    * Add action button and Poppins font import
    
    * Add recipe view
    
    * Add pliancy
    
    * Change background color in App.tsx
    
    * Increase horizontal margin for the whole view
    
    * Restore the App.tsx
    
    * Updated yarn.lock

---
+++ src/pages/RecipePage.tsx
@@ -0,0 +1,40 @@
+import { IonIcon } from '@ionic/react';
+import { arrowBackOutline, chevronDown, filterCircle } from 'ionicons/icons';
+
+export const RecipePage = () => {
+	return (
+		<div className="mx-16">
+			{/* Back Button */}
+			<div className="inline-flex flex-row items-center my-10 text-beige-1400 font-semibold text-base hover:text-beige-1000 hover:duration-300 cursor-pointer">
+				<IonIcon icon={arrowBackOutline} size="large" />
+				Tillbaka
+			</div>
+
+			{/* Header */}
+			<div className="flex flex-row items-center justify-between">
+				<div className="flex flex-row items-center space-x-24">
+					<p className="font-bold text-3xl">Receptförslag</p>
+					<p className="">
+						Dina ingredienser gav <strong>11</strong> matchningar
+					</p>
+				</div>
+
+				<div className="flex flex-row items-center space-x-24">
+					{/* Sorting */}
+					<div className="flex flex-row space-x-2 items-center space-x text-beige-1200 hover:text-beige-1000 hover:duration-300 cursor-pointer text-sm">
+						<p>Sortera efter</p>
+						<IonIcon icon={chevronDown} />
+					</div>
+					{/* Filter */}
+					<IonIcon
+						className="text-beige-1200 hover:text-beige-1000 cursor-pointer hover:duration-300"
+						icon={filterCircle}
+						size="large"
+					/>
+				</div>
+
+				{/* Add list of cards */}
+			</div>
+		</div>
+	);
+};
