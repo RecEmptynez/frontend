5880b350dd941eb5414ecf03b05e06645ea83ecc
Philip Winsnes <philip.winsnes@icloud.com>
2023-03-16 18:08:41 +0100

    Dev/rec 65 (#9)
    * Add action button and Poppins font import
    
    * Add recipe view
    
    * Add pliancy
    
    * Change background color in App.tsx
    
    * Increase horizontal margin for the whole view
    
    * Restore the App.tsx
    
    * Updated yarn.lock
    
    * REC-65: Add visible list on hover
    
    * REC-65: Add interactions for the filters
    
    * REC-65: Add fully functional filtering
    
    * Update App.tsx

--- src/pages/RecipePage.tsx
+++ src/pages/RecipePage.tsx
@@ -1,16 +1,18 @@
 import { IonIcon } from '@ionic/react';
-import { arrowBackOutline, chevronDown, filterCircle } from 'ionicons/icons';
+import {
+	arrowBackOutline,
+	chevronDown,
+} from 'ionicons/icons';
+import { Filtering } from '../components/recipe_components/Filtering';
 
 export const RecipePage = () => {
 	return (
 		<div className="mx-16">
-			{/* Back Button */}
 			<div className="inline-flex flex-row items-center my-10 text-beige-1400 font-semibold text-base hover:text-beige-1000 hover:duration-300 cursor-pointer">
 				<IonIcon icon={arrowBackOutline} size="large" />
 				Tillbaka
 			</div>
 
-			{/* Header */}
 			<div className="flex flex-row items-center justify-between">
 				<div className="flex flex-row items-center space-x-24">
 					<p className="font-bold text-3xl">Receptförslag</p>
@@ -20,16 +22,12 @@ export const RecipePage = () => {
 				</div>
 
 				<div className="flex flex-row items-center space-x-24">
-					{/* Sorting */}
 					<div className="flex flex-row space-x-2 items-center space-x text-beige-1200 hover:text-beige-1000 hover:duration-300 cursor-pointer text-sm">
 						<p>Sortera efter</p>
 						<IonIcon icon={chevronDown} />
 					</div>
-					{/* Filter */}
-					<IonIcon
-						className="text-beige-1200 hover:text-beige-1000 cursor-pointer hover:duration-300"
-						icon={filterCircle}
-						size="large"
+					<Filtering
+						filters={['Vegetariskt', 'Veganskt', 'Glutenfritt', 'Laktosfritt']}
 					/>
 				</div>
 
