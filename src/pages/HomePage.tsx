ed5546077eea06f3ffcaf04fdf0293c7e0f2247c
Philip Winsnes <philip.winsnes@icloud.com>
2023-03-23 14:42:59 +0100

    REC-23: Optimized the HomePage code

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -8,70 +8,43 @@ import { SearchRecipes } from "../api/SearchRecipes";
 import { fetchAllIngredients } from "../api/SearchIngredients";
 
 export const HomePage = () => {
-	const [availableIngredients, setAvailableIngredients] = useState<string[]>(
-		[]
-	);
+	const [availableIngredients, setAvailableIngredients] = useState<string[]>([]);
 	const [chosenIngredients, setChosenIngredients] = useState<string[]>([]);
 	const bottomDivRef = useRef<HTMLDivElement>(null);
 
 	useEffect(() => {
-		console.log("Mounting Home Page");
-		return () => {
-			console.log("Unmounting Home Page");
-			fetchAllIngredients().then((response) =>
-				setAvailableIngredients(response)
-			);
-		};
+		fetchAllIngredients().then(setAvailableIngredients);
 	}, []);
 
 	const addIngredient = (ingredient: string) => {
-		const newAvailableList = availableIngredients.filter(
-			(availableIngredient) => availableIngredient !== ingredient
-		);
-		setAvailableIngredients(newAvailableList);
-
-		const newChosenList = chosenIngredients;
-		newChosenList.push(ingredient);
-		setChosenIngredients(newChosenList);
+		setAvailableIngredients(availableIngredients.filter((i) => i !== ingredient));
+		setChosenIngredients([...chosenIngredients, ingredient]);
 	};
 
 	const removeIngredient = (ingredient: string) => {
-		const newChosenList = chosenIngredients.filter(
-			(chosenIngredient) => chosenIngredient !== ingredient
-		);
-		setChosenIngredients(newChosenList);
-
-		const newAvailableList = availableIngredients;
-		newAvailableList.push(ingredient);
-		setAvailableIngredients(newAvailableList);
+		setChosenIngredients(chosenIngredients.filter((i) => i !== ingredient));
+		setAvailableIngredients([...availableIngredients, ingredient]);
 	};
 
 	return (
 		<div className="h-screen overflow-x-hidden overscroll-none">
 			<div className="relative w-screen h-[200vh] bg-beige-200">
 				<div className="relative w-full h-screen flex flex-col items-center justify-center text-beige-1200 z-10">
-					<h1 className="font-bold text-[28px] select-none">
-						Vad ska jag laga för mat?
-					</h1>
+					<h1 className="font-bold text-[28px] select-none">Vad ska jag laga för mat?</h1>
 					<p className="w-1/3 text-center my-8 select-none">
-						Har du svårt att veta vad du ska laga idag? Då har du kommit rätt.
-						Vi hjälper dig ta vara på dina livsmedel genom att föreslå recept
-						utifrån ingredienser du har hemma.
+						Har du svårt att veta vad du ska laga idag? Då har du kommit rätt. Vi hjälper dig ta
+						vara på dina livsmedel genom att föreslå recept utifrån ingredienser du har hemma.
 					</p>
 					<ActionButton
 						label={"Kom igång"}
-						onClickAction={() =>
-							bottomDivRef.current?.scrollIntoView({ behavior: "smooth" })
-						}
+						onClickAction={() => bottomDivRef.current?.scrollIntoView({ behavior: "smooth" })}
 					/>
 				</div>
 				<div className="relative w-full h-screen flex flex-col justify-center text-beige-1200 z-10">
 					<div className="w-full h-full flex pt-32">
 						<div className="w-1/2 h-1/2 flex justify-center">
 							<div className="w-2/3 h-full flex flex-col items-center">
-								<h1 className="font-bold text-[28px] mb-7 select-none">
-									Sök ingrediens
-								</h1>
+								<h1 className="font-bold text-[28px] mb-7 select-none">Sök ingrediens</h1>
 								<SearchBar
 									availableIngredients={availableIngredients}
 									addIngredient={addIngredient}
@@ -79,9 +52,7 @@ export const HomePage = () => {
 							</div>
 						</div>
 						<div className="w-1/2 h-full flex flex-col items-center">
-							<h1 className="font-bold text-[28px] mb-7 select-none">
-								Valda ingredienser
-							</h1>
+							<h1 className="font-bold text-[28px] mb-7 select-none">Valda ingredienser</h1>
 							<IngredientsList
 								ingredients={chosenIngredients}
 								removeIngredient={removeIngredient}
