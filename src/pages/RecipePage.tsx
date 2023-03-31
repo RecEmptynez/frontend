48dd66f13a0ef3d0f0e5a852305501c310ae56eb
Philip Winsnes <philip.winsnes@icloud.com>
2023-03-31 11:12:47 +0200

    REC-84: Able to sort by relevance and ratings.

--- src/pages/RecipePage.tsx
+++ src/pages/RecipePage.tsx
@@ -1,56 +1,79 @@
 import { IonIcon } from "@ionic/react";
-import { arrowBackOutline} from "ionicons/icons";
+import { arrowBackOutline } from "ionicons/icons";
 import { useEffect, useState } from "react";
-import { useNavigate, useLocation} from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import { SearchRecipes } from "../api/SearchRecipes";
 import { RecipeCard } from "../components/RecipeCard";
-import { Filtering } from "../components/recipe_components/Filtering";
 import { Sorting } from "../components/recipe_components/Sorting";
 
-
 export const RecipePage = () => {
-  const navigate = useNavigate();
-  const location = useLocation();
-  const [matches, setMatches] = useState(0)
-	const [recipeCards, setRecipeCards] = useState(<></>)
-  
-  useEffect(() => {
-    let recipes: [any, any][];
-    let recipeCardsMap: any;
-    SearchRecipes(location.state.ingredients).then((response) => recipes = Object.entries(response["recipe_names"]))
-      .then(() => recipeCardsMap = recipes.map(([key, value]) => <RecipeCard title={key}
-      imageURL={value['picture_url']}
-      recipeURL={value['url']} difficulty={value['difficulty']} rating={value['rating']}
-      IngredientsHave={value['owned']} IngredientsNeed={value['total']}/>
-      )).then(() => setRecipeCards(recipeCardsMap)).then(() => setMatches(recipeCardsMap.length));
-  }, [location])
+	const navigate = useNavigate();
+	const location = useLocation();
+	const [recipes, setRecipes] = useState<[string, any][]>([]);
+	const [recipeCards, setRecipeCards] = useState<JSX.Element[]>([]);
 
-  return (
-    <div className="mx-16 h-full">
-      <div
-        onClick={() => navigate("/")}
-        className="inline-flex flex-row items-center my-10 text-beige-1400 font-semibold text-base hover:text-beige-1000 hover:duration-300 cursor-pointer"
-      >
-        <IonIcon icon={arrowBackOutline} size="large" />
-        Tillbaka
-      </div>
+	useEffect(() => {
+		const fetchRecipes = async () => {
+			const response = await SearchRecipes(location.state.ingredients);
+			const recipes = Object.entries(response.recipe_names);
+			setRecipes(recipes);
+			renderCards(recipes);
+		};
+		fetchRecipes();
+	}, [location.state.ingredients]);
 
-      <div className="flex flex-row items-center justify-between">
-        <div className="flex flex-row items-center space-x-24">
-          <p className="font-bold text-3xl">Receptförslag</p>
-          <p className="">
-            Dina ingredienser gav <strong>{matches}</strong> matchningar
-          </p>
-        </div>
+	const sortRecipes = (sortBy: string) => {
+		let sortedRecipes = [...recipes];
+		if (sortBy === "Svårighetsgrad") {
+			sortedRecipes.sort((a, b) => a[1].difficulty - b[1].difficulty);
+		} else if (sortBy === "Betyg") {
+			sortedRecipes.sort((a, b) => b[1].rating - a[1].rating);
+		}
+		renderCards(sortedRecipes);
+	};
 
-        <div className="flex flex-row items-center space-x-24">
-          <Sorting />
-          <Filtering
-            filters={["Vegetariskt", "Veganskt", "Glutenfritt", "Laktosfritt"]}
-          />
-        </div>
-      </div>
-      <div className="flex flex-wrap">{[recipeCards]}</div>
-    </div>
-  );
+	const renderCards = (recipes: [string, any][]) => {
+		setRecipeCards(
+			recipes.map(([key, value]) => (
+				<RecipeCard
+					key={key}
+					title={key}
+					imageURL={value.picture_url}
+					recipeURL={value.url}
+					difficulty={value.difficulty}
+					rating={value.rating}
+					IngredientsHave={value.owned}
+					IngredientsNeed={value.total}
+				/>
+			))
+		);
+	};
+
+	return (
+		<div className="mx-16 h-full">
+			<div
+				onClick={() => navigate("/")}
+				className="inline-flex flex-row items-center my-10 text-beige-1400 font-semibold text-base hover:text-beige-1000 hover:duration-300 cursor-pointer"
+			>
+				<IonIcon icon={arrowBackOutline} size="large" />
+				Tillbaka
+			</div>
+			<div className="flex flex-row items-center justify-between">
+				<div className="flex flex-row items-center space-x-24">
+					<p className="font-bold text-3xl">Receptförslag</p>
+					<p className="">
+						Dina ingredienser gav <strong>{recipes.length}</strong> matchningar
+					</p>
+				</div>
+				<div className="flex flex-row items-center space-x-24">
+					<Sorting
+						sortings={["Relevans", "Svårighetsgrad", "Betyg"]}
+						initialSorting={"Relevans"}
+						onChange={sortRecipes}
+					/>
+				</div>
+			</div>
+			<div className="flex flex-wrap">{recipeCards}</div>
+		</div>
+	);
 };
