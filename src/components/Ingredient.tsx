d46c823d36f80713678d8edd19245e4f8e59e1b8
Jonsson01 <emijon01@gmail.com>
2023-04-17 11:06:56 +0200

    Add firebase

--- src/components/Ingredient.tsx
+++ src/components/Ingredient.tsx
@@ -2,20 +2,20 @@ import { add } from "ionicons/icons";
 import { IonIcon } from "@ionic/react";
 
 export interface IngredientProp {
-	name: string;
-	addIngredient: (Ingredient: string) => void;
+  name: string;
+  addIngredient: (Ingredient: string) => void;
 }
 
 export const Ingredient = (props: IngredientProp) => {
-	const { name, addIngredient } = props;
+  const { name, addIngredient } = props;
 
-	return (
-		<div
-			className={`py-2 px-3 flex flex-row space-x-4 items-center text-left hover:bg-beige-600 rounded-md text-beige-1200 active:text-beige-1000 cursor-pointer hover:ease-out hover:duration-300 active:ease-out active:duration-300`}
-			onClick={() => addIngredient(name)}
-		>
-			<IonIcon icon={add} size="small" />
-			<p className="text-left select-none">{name}</p>
-		</div>
-	);
+  return (
+    <div
+      className={`py-2 px-3 flex flex-row space-x-4 items-center text-left hover:bg-beige-600 rounded-md text-beige-1200 active:text-beige-1000 cursor-pointer hover:ease-out hover:duration-300 active:ease-out active:duration-300`}
+      onClick={() => addIngredient(name)}
+    >
+      <IonIcon icon={add} size="small" />
+      <p className="text-left select-none">{name}</p>
+    </div>
+  );
 };
--- src/components/recipe_components/Filtering.tsx
+++ src/components/recipe_components/Filtering.tsx
@@ -4,62 +4,62 @@ import { filterCircle } from "ionicons/icons";
 import { ListItem } from "../recipe_components/ListItem";
 
 interface FilteringProps {
-	filters: string[];
+  filters: string[];
 }
 
 export const Filtering = ({ filters }: FilteringProps) => {
-	// Create a map which stores the status of the filters with initial values being false.
-	const [activeFilters, setActiveFilters] = useState<Map<string, boolean>>(
-		new Map(filters.map((key) => [key, false]))
-	);
-	const [isOpen, setIsOpen] = useState(false);
-	const listRef = useRef<HTMLUListElement>(null);
-	const buttonRef = useRef<HTMLIonIconElement>(null);
+  // Create a map which stores the status of the filters with initial values being false.
+  const [activeFilters, setActiveFilters] = useState<Map<string, boolean>>(
+    new Map(filters.map((key) => [key, false]))
+  );
+  const [isOpen, setIsOpen] = useState(false);
+  const listRef = useRef<HTMLUListElement>(null);
+  const buttonRef = useRef<HTMLIonIconElement>(null);
 
-	useEffect(() => {
-		console.log(activeFilters.entries());
-	}, [activeFilters]);
+  useEffect(() => {
+    console.log(activeFilters.entries());
+  }, [activeFilters]);
 
-	useEffect(() => {
-		const handleClickOutside = (e: MouseEvent) => {
-			if (
-				!listRef.current?.contains(e.target as Node) &&
-				!buttonRef.current?.contains(e.target as Node)
-			) {
-				setIsOpen(false);
-			}
-		};
-		document.addEventListener("click", handleClickOutside);
-		return () => document.removeEventListener("click", handleClickOutside);
-	}, [listRef, buttonRef]);
+  useEffect(() => {
+    const handleClickOutside = (e: MouseEvent) => {
+      if (
+        !listRef.current?.contains(e.target as Node) &&
+        !buttonRef.current?.contains(e.target as Node)
+      ) {
+        setIsOpen(false);
+      }
+    };
+    document.addEventListener("click", handleClickOutside);
+    return () => document.removeEventListener("click", handleClickOutside);
+  }, [listRef, buttonRef]);
 
-	const handleFilterChange = (filter: string, isActive: boolean) => {
-		setActiveFilters(new Map(activeFilters.set(filter, isActive)));
-	};
+  const handleFilterChange = (filter: string, isActive: boolean) => {
+    setActiveFilters(new Map(activeFilters.set(filter, isActive)));
+  };
 
-	return (
-		<div className="relative">
-			<IonIcon
-				className="text-beige-1200 hover:text-beige-1000 cursor-pointer"
-				icon={filterCircle}
-				size="large"
-				ref={buttonRef}
-				onClick={() => setIsOpen(!isOpen)}
-			/>
-			{isOpen && (
-				<ul
-					ref={listRef}
-					className="z-[1] p-3 space-y-1 bg-beige-200 rounded-xl absolute shadow-md right-0 top-12"
-				>
-					{/* {filters.map((filter) => (
+  return (
+    <div className="relative">
+      <IonIcon
+        className="text-beige-1200 hover:text-beige-1000 cursor-pointer"
+        icon={filterCircle}
+        size="large"
+        ref={buttonRef}
+        onClick={() => setIsOpen(!isOpen)}
+      />
+      {isOpen && (
+        <ul
+          ref={listRef}
+          className="z-[1] p-3 space-y-1 bg-beige-200 rounded-xl absolute shadow-md right-0 top-12"
+        >
+          {/* {filters.map((filter) => (
 						// <ListItem
 						// 	onChange={handleFilterChange}
 						// 	name={filter}
 						// 	checked={activeFilters.get(filter) || false}
 						// />
 					))} */}
-				</ul>
-			)}
-		</div>
-	);
+        </ul>
+      )}
+    </div>
+  );
 };
