3f2e4962bc5f1370db7fb698408ed3698d5b1bbf
Jonsson01 <emijon01@gmail.com>
2023-04-18 17:49:17 +0200

    Increase to 100 recipes max when searching

--- src/api/SearchRecipes.tsx
+++ src/api/SearchRecipes.tsx
@@ -1,10 +1,11 @@
 import { axiosClient } from "../apiClient";
 
-export function SearchRecipes(ingredients : string[]){
-    const result = axiosClient.post("/search/recipes", 
-        {
-            "max_num": 20,
-            "ingredient_names": ingredients
-        }).then((response) => response.data)
-    return result
-};
+export function SearchRecipes(ingredients: string[]) {
+  const result = axiosClient
+    .post("/search/recipes", {
+      max_num: 100,
+      ingredient_names: ingredients,
+    })
+    .then((response) => response.data);
+  return result;
+}
